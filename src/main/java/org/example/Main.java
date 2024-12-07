5f306f62c337ea1ee3d7f1fdf419a683327f21b5
DeePlusPlus <r2dan2d2@gmail.com>
2024-12-07 17:08:29 -0500

    Day 3 part 1.  Glad I can identify a regex problem.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,55 +4,31 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         try{
-            Integer countOfPassingReports = 0;
-            List<String> fileLinesAsList = Files.readAllLines(Paths.get("C:\\Users\\r2dan\\IdeaProjects\\aoc2024\\src\\main\\java\\org\\example\\day2_input.txt"));
-            List<List<Integer>> inputAs2DList = new ArrayList<List<Integer>>();
-            List<List<Integer>> inputAs2DListOfDifferences = new ArrayList<>();
-            //Get 2D list of input as integers
-            for(int i = 0; i < fileLinesAsList.size(); i ++){
-                List<Integer> lineInts = new ArrayList<>();
-                String line = fileLinesAsList.get(i);
-                String[] lineAsStringList = line.split(" ");
-                for(int j = 0; j < lineAsStringList.length; j++) {
-                    lineInts.add(Integer.parseInt(lineAsStringList[j]));
-                }
-                inputAs2DList.add(lineInts);
+            Integer runningSum = 0;
+            String regex = "mul\\(\\d+,\\d+\\)";
+            String wholeFile = Files.readAllLines(Paths.get("C:\\Users\\r2dan\\IdeaProjects\\aoc2024\\src\\main\\java\\org\\example\\day3_input.txt")).get(0);
+
+            //Parse out matches with regex
+            List<String> matches = new ArrayList<String>();
+            Matcher m = Pattern.compile("(?=(" + regex + "))").matcher(wholeFile);
+            while(m.find()) {
+                matches.add(m.group(1));
             }
 
-            //Convert list of integers to list of differences.  Should be easier to work with?
-            for(int i = 0; i < inputAs2DList.size(); i++) {
-                List<Integer> lineDifferences = new ArrayList<>();
-                for(int j = 0; j < inputAs2DList.get(i).size() - 1; j++){
-                    lineDifferences.add(inputAs2DList.get(i).get(j) - inputAs2DList.get(i).get(j + 1));
-                }
-                inputAs2DListOfDifferences.add(lineDifferences);
+            for(int i = 0; i < matches.size(); i++){
+                String[] subStrings = matches.get(i).split(",");
+                Integer firstNum = Integer.parseInt(subStrings[0].substring(4));
+                Integer secondNum = Integer.parseInt(subStrings[1].substring(0, subStrings[1].length() - 1));
+                runningSum += firstNum * secondNum;
             }
 
-            //Check each list of differences against rules
-            for(int i = 0; i < inputAs2DListOfDifferences.size(); i++){
-                Integer naturalSum = 0;
-                Integer absoluteSum = 0;
-                Boolean succeed = true;
-                for(int j = 0; j < inputAs2DListOfDifferences.get(i).size(); j++){
-                    Integer difference = inputAs2DListOfDifferences.get(i).get(j);
-                    if(difference < -3 || difference > 3 || difference == 0) {
-                        succeed = false;
-                        break;
-                    }
-                    naturalSum += difference;
-                    absoluteSum += Math.abs(difference);
-                }
-
-                if(Math.abs(naturalSum) == absoluteSum && succeed) {
-                    countOfPassingReports++;
-                }
-            }
-
-            System.out.print(countOfPassingReports);
+            System.out.print(runningSum);
         }
         catch (Exception e){
             System.out.print(e);
